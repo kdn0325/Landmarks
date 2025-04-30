e47d17fab6cb9faba997bdc144b57cd5263765cd
kdn0325 <dn10003@gmail.com>
2025-04-30 20:03:44 +0900

    add LandmarkDetail view to display detailed information about a selected landmark

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Abel on 4/30/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            Text(landmark.name)
+                .font(.title)
+
+            HStack {
+                Text(landmark.park)
+                    .font(.subheadline)
+                Spacer()
+                Text(landmark.state)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+            Divider()
+            Text("About \(landmark.name)")
+                .font(.title2)
+            Text(landmark.description)        }
+        .padding()
+
+        Spacer()
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
