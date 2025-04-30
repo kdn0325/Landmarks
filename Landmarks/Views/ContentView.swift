391d58ce50d4267d6b5ccf556d9b4c7d25dc83a4
kdn0325 <dn10003@gmail.com>
2025-04-30 20:03:40 +0900

    add ContentView to display the list of landmarks

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Abel on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
