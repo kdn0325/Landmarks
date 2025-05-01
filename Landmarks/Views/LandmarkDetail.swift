3676ac71c8c99a9827507a402a252142b20520c5
kdn0325 <dn10003@gmail.com>
2025-05-01 16:02:01 +0900

    refactor LandmarkDetail view layout for improved readability

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -11,30 +11,34 @@ struct LandmarkDetail: View {
     var landmark: Landmark
 
     var body: some View {
-        VStack {
+        ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            Text(landmark.name)
-                .font(.title)
+            VStack {
+                Text(landmark.name)
+                    .font(.title)
 
-            HStack {
-                Text(landmark.park)
-                    .font(.subheadline)
-                Spacer()
-                Text(landmark.state)
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+
+                HStack {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+                Divider()
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
             }
-            Divider()
-            Text("About \(landmark.name)")
-                .font(.title2)
-            Text(landmark.description)        }
-        .padding()
-
-        Spacer()
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
