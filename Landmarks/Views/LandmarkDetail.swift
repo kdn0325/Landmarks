d45fdf4abe5eb9a1b912eb438a20cd4291bb06d2
kdn0325 <dn10003@gmail.com>
2025-05-02 13:39:03 +0900

    refactor: enhance LandmarkDetail view with modelData bindings and structure

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
 
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+    }
+
     var body: some View {
+        @Bindable var modelData = modelData
+
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -18,18 +25,21 @@ struct LandmarkDetail: View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             VStack {
-                Text(landmark.name)
-                    .font(.title)
-
-
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
                     Text(landmark.state)
-                        .font(.subheadline)
-                        .foregroundStyle(.secondary)
                 }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
                 Divider()
                 Text("About \(landmark.name)")
                     .font(.title2)
@@ -43,5 +53,8 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: ModelData().landmarks[0])
+        .environment(modelData)
+
 }
